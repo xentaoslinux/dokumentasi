7105bf25745f6bfdbfe69c6f6829a71497aa4b73
Dindin Hernawan <dindin_hernawan@yahoo.com>
2018-09-18 21:34:21 +0700

    Menambahkan Partner ICWR

--- maintainer/update.sh
+++ maintainer/update.sh
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 repository=dokumentasi
-update="Update"
+update="Menambahkan Partner ICWR"
 figlet $repository
 cd ..
 echo "Set Remote $team..."
