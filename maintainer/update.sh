b369e8f133c44645ecf4e2f7fe1f9e6eb3dffd96
Dindin Hernawan (Xenta OS) <dindin_hernawan@yahoo.com>
2020-02-18 01:16:01 +0700

    Arsip Tue, 18 Feb 2020 01:15:52 +0700

--- maintainer/update.sh
+++ maintainer/update.sh
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 repository=dokumentasi
-update="Menambahkan E-Mail xentaoslinux@yahoo.com pada Halaman Donasi"
+update="Arsip Tue, 18 Feb 2020 01:15:52 +0700 "
 figlet $repository
 cd ..
 echo "Set Remote $team..."
